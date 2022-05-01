c7db781013120aabe03044061eabaabb5013ab09
HomelessGodYato <yaroslavbanchenko@gmail.com>
2022-05-01 22:03:29 +0200

    Log in, registration, activation, user, pages

--- pancakes/views.py
+++ pancakes/views.py
@@ -1,6 +1,97 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+
+from .forms import CreateUserForm
+from .tokens import account_activation_token
 
 
 def home_page(request):
     context = {"recipes": ['pancakes', 'donuts']}
     return render(request, 'home.html', context)
+
+
+def register_page(request):
+    if request.user.is_authenticated:
+        return redirect('user')
+    else:
+        form = CreateUserForm()
+        if request.method == "POST":
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                user = form.save(commit=False)
+                user.is_active = False
+                user.save()
+                current_site = get_current_site(request)
+                mail_subject = 'Activate your account.'
+                message = render_to_string('registration_login/acc_activation.html', {
+                    'user': user,
+                    'domain': current_site.domain,
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                    'token': account_activation_token.make_token(user),
+                })
+                to_email = form.cleaned_data.get('email')
+                email = EmailMessage(
+                    mail_subject, message, to=[to_email]
+                )
+                email.send()
+                return HttpResponse('Please confirm your email address to complete the registration')
+            else:
+                form = CreateUserForm()
+    context = {"form": form}
+    return render(request, 'registration_login/register.html', context)
+
+
+def activation(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        # return redirect('home')
+        return render(request, 'registration_login/registration_successful.html', {})
+    else:
+        return HttpResponse('Activation link is invalid!')
+
+
+def login_page(request):
+    context = {}
+    if request.user.is_authenticated:
+        return redirect('user')
+    else:
+        if request.method == "POST":
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+
+                return redirect('user')  # any page you want @user_page just for test
+            else:
+                messages.info(request, 'Username or password is incorrect')
+                return render(request, 'registration_login/login.html', context)
+
+        return render(request, 'registration_login/login.html', context)
+
+
+@login_required(login_url='login')
+def user_main_page(request):
+    return render(request, 'registration_login/user_main.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
