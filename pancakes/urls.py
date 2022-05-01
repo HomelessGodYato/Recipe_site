42f9cb250b1262fe57b700a501096853b571a031
HomelessGodYato <yaroslavbanchenko@gmail.com>
2022-05-01 21:49:11 +0200

    Log in, registration, activation, user, pages

--- pancakes/urls.py
+++ pancakes/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name="home")
+    path('', views.home_page, name="home"),
+    path('register/', views.register_page, name="register"),
+    path('login/', views.login_page, name="login"),
+    path('logout/', views.logout_user, name="logout"),
+    path('user/', views.user_main_page, name="user"),
+    path('activate/<uidb64>[0-9A-Za-z_\-]+/<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20}/$',
+         views.activation, name='activate'),
 ]
