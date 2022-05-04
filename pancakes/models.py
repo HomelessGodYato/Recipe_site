fae1e25fda7e551ab3d9a2e58e41a6ab198f4847
GrubeB <167775@stud.prz.edu.pl>
2022-05-04 02:54:44 +0200

    modles

--- pancakes/models.py
+++ pancakes/models.py
@@ -1,5 +1,48 @@
+import pathlib
 from django.db import models
-from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
 
+def recipe_image_upload_handler(instance, file_name):
+    file_path = pathlib.Path(file_name)
+    new_file_name = str(uuid.uuid1())
+    return f"uploads/{new_file_name}{file_path.suffix}"
+
+
+class RecipeImage(models.Model):
+    image = models.ImageField(upload_to=recipe_image_upload_handler)
+
+
+class RecipeCategory(models.Model):
+    name = models.CharField(max_length=254)
+
+
+class Recipe(models.Model):
+    # author = models.ForeignKey(
+    #     settings.AUTH_USER_MODEL,
+    #     on_delete=models.CASCADE,
+    # )
+    date_create = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=254)
+    cooking_time = models.IntegerField()
+    image = models.ForeignKey(to=RecipeImage, on_delete=models.CASCADE, null=True, blank=True)
+    categories = models.ManyToManyField(to=RecipeCategory, through='RecipeRecipeCategory')
+
+
+class RecipeRecipeCategory(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    name = models.ForeignKey(RecipeCategory, on_delete=models.CASCADE)
+
+
+class RecipeStage(models.Model):
+    recipe = models.ForeignKey(to=Recipe, on_delete=models.CASCADE, null=False)
+    cooking_time = models.IntegerField()
+    description = models.CharField(max_length=8191)
+    image = models.ForeignKey(to=RecipeImage, on_delete=models.CASCADE, null=True, blank=True)
+
+
+class Ingredient(models.Model):
+    recipe_stage = models.ForeignKey(to=RecipeStage, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=254)
+    unit = models.CharField(max_length=254)
+    amount = models.CharField(max_length=254)
